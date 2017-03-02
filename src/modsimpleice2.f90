ce25335bbc2094d013b0759d192911d3a281e3e7
Fredrik Jansson <fjansson@abo.fi>
2017-03-02 09:59:51 +0100

    merge precipitation function v2

--- src/modsimpleice2.f90
+++ src/modsimpleice2.f90
@@ -118,8 +118,9 @@ module modsimpleice2
 
 !> Calculates the microphysical source term.
   subroutine simpleice
-    use modglobal, only : i1,j1,k1,rdt,rk3step,timee,rlv,cp,tup,tdn,pi,tmelt
+    use modglobal, only : i1,j1,k1,rdt,rk3step,timee,rlv,cp,tup,tdn,pi,tmelt,kmax,dzf,dzh
     use modfields, only : sv0,svm,svp,qtp,thlp,qt0,ql0,exnf,rhof,tmp0,rhobf,qvsl,qvsi,esl
+    
     use modsimpleicestat, only : simpleicetend
     implicit none
     integer:: i,j,k
